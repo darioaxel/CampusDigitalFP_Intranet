f29e1e4d168de63f6ec4f41aa991ccb26ef1b718
darioaxel <darioaxel@gmail.com>
2026-02-25 18:49:12 +0100

    fix: layout con variante de tema

--- app/layouts/dashboard.vue
+++ app/layouts/dashboard.vue
@@ -17,7 +17,7 @@ definePageMeta({
   <SidebarProvider>
     <AppSidebar />
     <SidebarInset>
-      <header class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <header class="flex h-16 shrink-0 items-center justify-between gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
         <div class="flex items-center gap-2 px-4">
           <SidebarTrigger class="-ml-1" />
           <Separator
@@ -25,7 +25,9 @@ definePageMeta({
             class="mr-2 data-[orientation=vertical]:h-4"
           />
           <DynamicBreadCrumb :url="useRoute().path" />
-            
+        </div>
+        <div class="flex items-center gap-2 px-4">
+          <ThemeToggle />
         </div>
       </header>
        <main class="flex-1">
